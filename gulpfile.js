3765ca632cad8d45ebfc0139afebafe3a32c2407
Shweta Bansal <ershwetabansal@gmail.com>
2017-01-18 22:28:42 +0000

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var runSequence = require('run-sequence');
 var webpack = require('webpack-stream');
